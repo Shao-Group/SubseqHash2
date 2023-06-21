84f99a344e3b0218ed3e3dbdff82a099d84ed6e2
Xiang Li <xpl5168@psu.edu>
2023-06-21 04:28:03 +0000

    add syncmer

---
+++ syncmerseeding.cpp
@@ -0,0 +1,103 @@
+#include "syncmerseeding.h"
+
+
+uint64_t syncmerseeding::murmur64(kmer key)
+{    
+    uint64_t h = (uint64_t) key;
+    h+=1;
+    h ^= (h >> 33);
+    h *= 0xff51afd7ed558ccdL;
+    h ^= (h >> 33);
+    h *= 0xc4ceb9fe1a85ec53L;
+    h ^= (h >> 33);
+    return h;
+}
+
+void syncmerseeding::get_syncmers(std::string str, std::vector<seed>& seeds)
+{
+    size_t len = str.length();
+    std::list<MMentry> que;
+
+    kmer mask = (1ULL<<(s<<1)) - 1;
+    kmer mask_k = (1ULL<<(k<<1)) - 1;
+    char cur[k];
+
+    str.copy(cur, s, 0);
+    kmer now = encode(cur, s);
+    uint64_t v = murmur64(now);
+
+    str.copy(cur, k, 0);
+    kmer strval = encode(cur, k);
+
+    que.push_back((MMentry){v, 0, now});
+
+    int num = k - s + 1;
+
+    for(size_t i = 1; i < num; i++)
+    {
+        now = (now<<2) & mask;
+        now |= alphabetIndex(str[i+s-1]);
+        v = murmur64(now);
+
+        while(!que.empty() && v <= que.back().hash)
+            que.pop_back();
+
+        que.push_back((MMentry){v, i, now});
+    }
+
+    for(size_t i = k-s+1; i <= len - s; i++)
+    {       
+        while(!que.empty() && que.front().pos < i - num)
+            que.pop_front();
+
+        for(int j:pos)
+            if(que.front().pos - (i-num) == j)
+            {
+                seed tmp;
+
+                tmp.st = i - num;
+                tmp.ed = tmp.st + k - 1;
+                tmp.hashval = strval;
+
+                seeds.push_back(tmp);
+            }
+
+        now = (now<<2) & mask;
+        now |= alphabetIndex(str[i+s-1]);
+        v = murmur64(now);
+
+        strval = (strval<<2) & mask_k;
+        strval |= alphabetIndex(str[i+s-1]);
+
+        // char* tmp1 = (char*)malloc(sizeof(char) *k), *tmp2= (char*)malloc(sizeof(char) *k);
+        // decode(now, s, tmp1);
+        // decode(strval, k, tmp2);
+        // for(int z = 0; z<s; z++)
+        //     printf("%c", tmp1[z]);
+        // printf(" ");
+        // for(int z = 0; z<k; z++)
+        //     printf("%c", tmp2[z]);
+        // printf("\n");
+        // printf("%s\n", str.substr(i - num + 1, k).c_str());
+
+        while(!que.empty() && v <= que.back().hash)
+            que.pop_back();
+
+        que.push_back((MMentry){v, i, now});
+    }        
+
+    while(!que.empty() && que.front().pos < len - k)
+        que.pop_front();
+
+    for(int j:pos)
+        if(que.front().pos - (len - k) == j)
+        {
+            seed tmp;
+
+            tmp.st = len - k;
+            tmp.ed = len - 1;
+            tmp.hashval = strval;
+
+            seeds.push_back(tmp);
+        }
+}
---
+++ syncmerseeding.h
@@ -0,0 +1,48 @@
+/*
+  minimap2-style syncmer seeding.
+
+  By: Ke, Xiang@PSU
+  Last edited: 06/15/2023
+*/
+
+#include "seeding.h"
+#include "util.h"
+#include <cstdint>
+#include <inttypes.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <list>
+
+#ifndef _SYNCMERSEEDING_H
+#define _SYNCMERSEEDING_H
+
+struct MMentry{
+	uint64_t hash;
+	size_t pos;
+	kmer str;
+};
+
+class syncmerseeding: private seeding
+{
+
+private:
+
+	uint64_t murmur64(kmer key);
+	int s;
+	std::vector<int> pos;
+public:
+
+	syncmerseeding(int k1, int s1): seeding(k1, k1)
+	{
+		s = s1;
+	}
+	void add(int p)
+	{
+		pos.push_back(p);
+	}
+
+	void get_syncmers(std::string s, std::vector<seed>& seeds);
+
+};
+#endif
