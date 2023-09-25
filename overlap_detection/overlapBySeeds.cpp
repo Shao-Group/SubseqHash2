5a92548cfc7fa1165889e6c62b3eef9c7328b7de
Ke Chen <kxc5915@psu.edu>
2023-09-25 18:28:05 -0400

    fix bug for wrong str_rc when storing seeds

---
+++ overlap_detection/overlapBySeeds.cpp
@@ -0,0 +1,140 @@
+/*
+  Given a set of seed files (readable by loadSeedsUnordered), output pairs of reads
+  with the number of unique seeds they share.
+  
+  By: Ke@PSU
+  Last edited: 09/19/2023
+*/
+
+#include "../util.h"
+#include <sys/stat.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+/*
+  An upper diagonal matrix without the main diagonal,
+  valid indices for Table::access(i, j) are 1<=i<j<=n
+*/
+class Table{
+    size_t n;
+    unsigned int* arr;
+
+public:
+    Table(size_t n);
+    ~Table();
+    unsigned int& access(size_t i, size_t j);
+    void saveNoneZeroEntries(const char* filename);
+};
+
+int main(int argc, const char * argv[])    
+{   
+    if(argc != 6){
+	printf("usage: overlapBySeeds.out seedsDir k repeatId numReads fileExt\nrepeatId should be between 0 and floor((k-1)/2), seeds from both repeatId and k-1-repeatId are used\n");
+	return 1;
+    }
+
+    int k = atoi(argv[2]);
+    int r = atoi(argv[3]);
+    int n = atoi(argv[4]);
+
+    int r2 = k-1-r;
+    if(r > r2){
+	int tmp = r2;
+	r2 = r;
+	r = tmp;
+    }else if(r==r2){
+	r2 = -1; //self symmetric, no need to load twice
+    }
+
+    char filename[500];
+    int i = strlen(argv[1]);
+    memcpy(filename, argv[1], i);
+    if(filename[i-1] != '/'){
+	filename[i] = '/';
+	++i;
+    }
+    
+    unordered_map<kmer, vector<size_t>, kmerHash> all_seeds;
+    int j;
+    
+    struct stat test_file;
+    for(j=1; j<=n; j+=1){
+	sprintf(filename+i, "%d-%d.%s", r, j, argv[5]);
+	if(stat(filename, &test_file) != 0){//seed file does not exist
+	    fprintf(stderr, "Stopped, cannot find file %d-%d.%s\n", r, j, argv[5]);
+	    break;
+	}
+	loadSeedsUnordered(filename, j, all_seeds);
+    }
+
+    if(r2 >= 0){
+	for(j=1; j<=n; j+=1){
+	    sprintf(filename+i, "%d-%d.%s", r2, j, argv[5]);
+	    if(stat(filename, &test_file) != 0){//seed file does not exist
+		fprintf(stderr, "Stopped, cannot find file %d-%d.%s\n", r2, j, argv[5]);
+		break;
+	    }
+	    loadSeedsUnordered(filename, j, all_seeds);
+	}
+    }
+
+    sprintf(filename+i, "overlap-n%d-r%d.all-pair", n, r);
+
+    Table share_ct(n);
+
+    int a, b, c;
+    
+    for(auto& seed : all_seeds){
+	c = seed.second.size();
+	for(i=0; i<c; ++i){
+	    a = seed.second[i];
+	    for(j=i+1; j<c; ++j){
+		b = seed.second[j];
+		++ share_ct.access(a, b);
+		/*
+		if(share_ct.access(a, b) == threshold){
+		    fprintf(fout, "%d %d\n", a, b);
+		}
+		*/
+	    }
+	}
+    }
+
+    share_ct.saveNoneZeroEntries(filename);
+    
+    return 0;
+}
+
+Table::Table(size_t n): n(n){
+    size_t size = (n*(n-1))>>1;
+    arr = new unsigned int[size];
+    memset(arr, 0, sizeof *arr * size);
+}
+
+Table::~Table(){
+    delete[] arr;
+}
+
+unsigned int& Table::access(size_t i, size_t j){
+    return arr[((((n<<1)-i)*(i-1))>>1)+j-i-1];
+}
+
+void Table::saveNoneZeroEntries(const char* filename){
+    FILE* fout = fopen(filename, "w");
+
+    size_t i, j, k, size=(n*(n-1))>>1;
+    for(k=0, i=1, j=2; k<size; ++k){
+	if(arr[k] > 0){
+	    fprintf(fout, "%zu %zu %u\n", i, j, arr[k]);
+	}
+	j += 1;
+	if(j > n){
+	    i += 1;
+	    j = i + 1;
+	}
+    }
+    
+    fclose(fout);
+}
